ca6e200439f813ec18850cc635a2bb09b8eb5f9b
ArkzioPrimeReborn <xigaaa12138@gmail.com>
2025-11-25 18:57:32 +0800

    Change Prebuilt Kernel Build to OSS Kernel Build Use MT6983 OSS Kernel Base on MT6895 OSS Kernel https://github.com/ProjectRubens/android_kernel_xiaomi_mt6895
    feat: Adapt OSS Kernel for MTK Dimensity 9000
    
    Complete OSS Kernel adaptation for MTK Dimensity 9000 (MT6983) chip, modified files list:
    - arch/arm64/configs/vendor/matisse.config
    - arch/arm64/configs/vendor/xiaomi_mt6983.config
    - arch/arm64/configs/xiaomi_mt6983.config
    - drivers/gpu/mediatek/mali-valhall/gpu/arm/midgard/Makefile
    - drivers/gpu/mediatek/mali-valhall/gpu/arm/midgard/csf/mali_kbase_csf_csg_debugfs.c
    - drivers/gpu/mediatek/mali-valhall/gpu/arm/midgard/mali_kbase_as_fault_debugfs.c
    - drivers/gpu/mediatek/mali-valhall/gpu/arm/midgard/mali_kbase_regs_history_debugfs.c
    - drivers/gpu/mediatek/mali-valhall/gpu/arm/midgard/mali_kbase_regs_history_debugfs.h
    - sound/soc/et7480/Makefile

--- drivers/gpu/mediatek/mali-valhall/gpu/arm/midgard/csf/mali_kbase_csf_csg_debugfs.c
+++ drivers/gpu/mediatek/mali-valhall/gpu/arm/midgard/csf/mali_kbase_csf_csg_debugfs.c
@@ -365,7 +365,7 @@ static int kbasep_csf_queue_group_debugfs_show(struct seq_file *file,
 	seq_printf(file, "MALI_CSF_CSG_DEBUGFS_VERSION: v%u\n",
 			MALI_CSF_CSG_DEBUGFS_VERSION);
 
-	mutex_lock(&kctx->csf.lock);
+	rt_mutex_lock(&kctx->csf.lock);
 	kbase_csf_scheduler_lock(kbdev);
 	for (gr = 0; gr < MAX_QUEUE_GROUP_NUM; gr++) {
 		struct kbase_queue_group *const group =
@@ -375,7 +375,7 @@ static int kbasep_csf_queue_group_debugfs_show(struct seq_file *file,
 			kbasep_csf_scheduler_dump_active_group(file, group);
 	}
 	kbase_csf_scheduler_unlock(kbdev);
-	mutex_unlock(&kctx->csf.lock);
+	rt_mutex_unlock(&kctx->csf.lock);
 
 	return 0;
 }
